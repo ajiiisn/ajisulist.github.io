41890d822070c75145637a9d082dac4c611bedbb
Aji Sulistyo Nugroho <ajisulist@gmail.com>
2022-01-28 23:33:47 +0000

    Update via CircleCI

---
+++ _next/static/ksXvJqVGdgFOeelXw5lci/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/649-b39c8d69385b5bc3.js","static/chunks/pages/index-c2907a8141159acb.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/z2FJxdgxjYYuz8Smcb-hR/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/649-b39c8d69385b5bc3.js","static/chunks/pages/index-2a009f7e4e0fa443.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
