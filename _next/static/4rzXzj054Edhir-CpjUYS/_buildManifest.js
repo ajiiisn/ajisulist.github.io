e1cf390bc17461cd2c2d1a3f42f2bad4c80504af
Aji Sulistyo Nugroho <ajisulist@gmail.com>
2022-03-25 22:59:46 +0000

    Update via CircleCI

--- _next/static/4rzXzj054Edhir-CpjUYS/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/649-966ee5d7bceec2de.js","static/chunks/pages/index-c2907a8141159acb.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
